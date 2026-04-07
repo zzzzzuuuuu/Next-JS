9d0adee3924966e6bf3cef3b6cb002a5f3c6b6a3
zzzzzuuuuu <agu0830@naver.com>
2026-04-07 16:23:31 +0900

    chore: 프리티어 설정

---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  semi: true,
+  printWidth: 100,
+  singleQuote: true,
+  arrowParens: 'avoid',
+  endOfLine: 'crlf',
+  quoteProps: 'as-needed',
+  tabWidth: 2,
+  useTabs: false,
+  trailingComma: 'none'
+};
